d280ed44c5c1b2806c86e996d254d854c3f34388
MinoDentori <45499568+MinoDentori@users.noreply.github.com>
2020-03-28 10:44:57 +0200

    Add files via upload
    Homework for lesson 5 to OSTPC cousre

---
+++ OSTPC_Less5_Home/OSTPC_Less5_Home/Order.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSTPC_Less5_Home
+{
+   public class Order
+    {
+        private List<string> personListOfDishes;
+
+        public string PersonName { get; set; }
+        public Order() {
+            PersonName = GetPersonName();
+            personListOfDishes = GetDishesToList();
+        }
+        public Order(string personName, List<string> personListOfDishes)
+        {
+            PersonName = personName;
+            this.personListOfDishes = personListOfDishes;
+        }
+        private string GetPersonName() {
+            string personName;
+            do
+            {
+                Console.WriteLine("Print person's name please.");
+                personName = Console.ReadLine();
+            } while (IsStringInputedCorrectly(personName) == false);
+            return personName;
+        }
+        private List<string> GetDishesToList() {
+            List<string> currentDishes = new List<string>();
+            string controllWord = "Yes";
+            string currentDish = "";
+            while (controllWord == "Yes") {
+                do
+                {
+                    Console.WriteLine("Print the name of dishes, that you want add to order.");
+                    currentDish = Console.ReadLine();
+                } while (IsStringInputedCorrectly(currentDish) == false);
+                do
+                {
+                    Console.WriteLine("Print 'Yes' to add external dish or 'No' to continue.");
+                    controllWord = Console.ReadLine();
+                } while (IsStringInputedCorrectly(controllWord) == false);
+                currentDishes.Add(currentDish);
+            }
+            return currentDishes;
+        }
+
+        public void PrintDishesFromOrder() {
+            foreach (string variable in personListOfDishes) {
+                Console.WriteLine($"-{variable}");
+            }
+        
+        }
+        private bool IsStringInputedCorrectly(string inputedString) {
+            if (String.IsNullOrWhiteSpace(inputedString) == true)
+            {
+                Console.WriteLine("You inputed string uncorrectly.");
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ OSTPC_Less5_Home/OSTPC_Less5_Home/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+namespace OSTPC_Less5_Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, welcome to our restaurant.");
+            int numberOfPerson = GetNumberOfPerson();
+            List<Order> currentOrders = new List<Order>();
+            currentOrders = GetAllOrders(numberOfPerson);
+            PrintAllOrders(currentOrders);
+        }
+
+        public static int GetNumberOfPerson() {
+            int inputedNumberOfPerson=0;
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Please, input a number of person, who will make an order.");
+                    inputedNumberOfPerson = int.Parse(Console.ReadLine());
+                } while (IsNumberInputedCorrectly(inputedNumberOfPerson) == false);
+                return inputedNumberOfPerson;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Please try again.");
+                return GetNumberOfPerson();
+            }
+        }
+
+        public static List<Order> GetAllOrders(int numberOfPerson) {
+            List<Order> currentOrderList = new List<Order>();
+            for (int i = 0; i<numberOfPerson; i++) {
+                Console.WriteLine($"Order for {i+1} person.");
+                Order currentOrder = new Order();
+                currentOrderList.Add(currentOrder);
+            }
+            return currentOrderList;
+        }
+
+        public static void PrintAllOrders(List<Order> currentOrdersList) {
+            foreach (Order variable in currentOrdersList) {
+                Console.WriteLine($"Order for {variable.PersonName}:");
+                variable.PrintDishesFromOrder();
+            }
+        
+        }
+        public static bool IsNumberInputedCorrectly(int inputedNumber) {
+            if (inputedNumber <= 0)
+            {
+                Console.WriteLine("Your input is uncorrectly. Please try again.");
+                return false;
+            }
+            return true;
+        }
+       
+    }
+   
+}
